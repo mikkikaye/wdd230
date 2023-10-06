036097b31cf21df0274c272d9a527d80eb56f25a
mikkikaye <mckenziej5@gmail.com>
2023-10-05 22:10:33 -0600

    updated autocomplete

--- scripts/passwordCheck.js
+++ scripts/passwordCheck.js
@@ -19,10 +19,9 @@ function checkSame() {
 }
 
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+var output = document.getElementById("value");
+output.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
   console.log(this);
