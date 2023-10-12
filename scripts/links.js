a936afa4e513c6e647554f3a010caa15b26ff2d9
mikkikaye <mckenziej5@gmail.com>
2023-10-11 19:39:38 -0600

    added dynamic links

--- scripts/links.js
+++ scripts/links.js
@@ -2,18 +2,43 @@ const baseURL = "https://mikkikaye.github.io/wdd230/";
 const linksURL = "https://mikkikaye.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const linksURL = await fetch(linksURL);
-    const data = await response.json(); 
-    console.log(data);
-    displayLinks(data);
+    try {
+        const response = await fetch(linksURL);
+        const weeksArr = await response.json(); 
+        displayLinks(weeksArr);
+    } catch (err) {
+        console.error("Error", err);
+    }
+}
+
+
+
+function displayLinks(weeksArr) {
+
+    const ulList = document.getElementById("ulList");
+    const arrWeeks = Array.from(weeksArr.weeks);
+
+    ulList.innerHTML = "";
+
+    arrWeeks.forEach((week) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${week.week}: `;
+
+
+        week.links.forEach((li, index) => {
+            const aLink = document.createElement('a');
+            aLink.setAttribute("href", li.url); 
+            aLink.textContent = `${li.title}`;
+            
+            listItem.appendChild(aLink);
+
+            if (index < week.links.length - 1) {
+                listItem.appendChild(document.createTextNode(" | "));
+            }
+        });
+        ulList.appendChild(listItem);
+        
+    });
 }
 
 getLinks();
-
-function displayLinks(weeks) {
-    weeks.forEach((week) => {
-        for (let i=0; i < links.length; i++) {
-            
-        }
-    });
-}
